3a0fdea04d349fe4775463463873aaa67d1307cb
mathznxs <math945374081@gmail.com>
2026-07-09 19:08:10 -0300

    Seção 8 - Aula 74

---
+++ src/com/mathdev/javacourse/aulas/secao8/aula74/application/Program.java
@@ -0,0 +1,25 @@
+package com.mathdev.javacourse.aulas.secao8.aula74.application;
+
+import com.mathdev.javacourse.aulas.secao8.aula74.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main (String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+
+        System.out.println("Dados dos produtos:");
+        System.out.print("Nome: ");
+        product.name = sc.nextLine();
+        System.out.print("Preço: R$");
+        product.price = sc.nextDouble();
+        System.out.print("Quantidade: ");
+        product.quantity = sc.nextInt();
+
+        System.out.printf("\nNome: %s;\nPreço: R$%.2f\nQuantidade: %d", product.name, product.price, product.quantity);
+    }
+}
