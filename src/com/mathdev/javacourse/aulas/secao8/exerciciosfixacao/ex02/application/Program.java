27ee44c5af53646b729c96da9b92e22fc5b8a2cf
mathznxs <math945374081@gmail.com>
2026-07-11 12:56:37 -0300

    Seção 8 - Exercício de Fixação 2

---
+++ src/com/mathdev/javacourse/aulas/secao8/exerciciosfixacao/ex02/application/Program.java
@@ -0,0 +1,4 @@
+package com.mathdev.javacourse.aulas.secao8.exerciciosfixacao.ex02.application;
+
+public class Program {
+}
---
+++ src/com/mathdev/javacourse/aulas/secao8/exerciciosfixacao/ex02/entities/Employee.java
@@ -0,0 +1,4 @@
+package com.mathdev.javacourse.aulas.secao8.exerciciosfixacao.ex02.entities;
+
+public class Employee {
+}
